efce742c2dc2f916bbe452f9325d98f22c1be334
Luke Paris <Paradoxis@users.noreply.github.com>
2017-09-27 21:42:21 +0200

    Reset indentation in gordon.sh
    Yeah the indentation war will never be won

--- gordon.sh
+++ gordon.sh
@@ -2,13 +2,13 @@
 # Copyright (c) 2017 - Paradoxis
 
 function gordon()
-	{
+{
     if [[  $? != 0 ]]; then
         insult=$(ls $GORDON_INSULTS | gshuf -n1)
         afplay $GORDON_INSULTS/$insult &
         disown
     fi
-	}
+}
 
 GORDON_INSULTS=~/.gordon
 PROMPT_COMMAND=gordon
